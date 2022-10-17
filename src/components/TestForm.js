39f2a6fdd651b2d0c323e6b1bb7200f617b483a9
MohammedVaraliya <varaliyamohammed1@gmail.com>
2022-10-17 23:49:33 +0530

    Add button remove new line

--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -13,6 +13,23 @@ export default function TestForm(props) {
         setText(result)
     }
 
+    const handleUpLower = () => {
+        let LoweText = text.toLocaleLowerCase();
+        setText(LoweText);
+    }
+
+    const handleUpNewLine = () => {
+        const regex = /\n/g;
+        const result = text.replace(regex, '');
+        setText(result)
+    }
+
+    const handleUpExtraSpace = () => {
+        const regex = /\s+/g;
+        const result = text.replace(regex, ' ');
+        setText(result)
+    }
+
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
@@ -26,7 +43,10 @@ export default function TestForm(props) {
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="12"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpUppercase}>Convert To Uppercase</button>
-                <button className="btn btn-primary" onClick={handleUpPunctuations}>Remove Punctuations</button>
+                <button className="btn btn-primary" onClick={handleUpLower}>Convert To Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleUpPunctuations}>Remove Punctuations</button>
+                <button className="btn btn-primary" onClick={handleUpNewLine}>Remove New Line</button>
+                <button className="btn btn-primary mx-2" onClick={handleUpExtraSpace}>Remove Extra Spaces</button>
             </div>
         </>
     )
