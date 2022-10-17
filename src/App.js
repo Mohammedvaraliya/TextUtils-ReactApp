33ec8e8d79a0e49e162fe2c17d405cf252ec28b8
MohammedVaraliya <varaliyamohammed1@gmail.com>
2022-10-17 21:26:08 +0530

    Add props to components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils"/>
+      <Navbar title="TextUtils" aboutText="About Us"/>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 
 export default function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,4 +27,14 @@ export default function Navbar(props) {
             </div>
         </nav>
     )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    aboutText: PropTypes.string.isRequired,
+}
+
+Navbar.defaultProps = {
+    title: "Set Title here",
+    aboutText: "About",
 }
