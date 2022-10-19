60a8a60daa6910b34efad626f5437cf8b335d3db
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-19 23:33:55 +0530

    Add More Functionality to textutils Home

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,10 +8,12 @@ export default function Alert(props) {
     }
 
     return (
-        props.alert &&
+        <div style={{height: "37px"}}>
+        {props.alert &&
         <> <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                 <strong>{capitalize(props.alert.type)}</strong> {props.alert.msg}.
             </div>
-        </>
+        </>}
+        </div>
     )
 }
