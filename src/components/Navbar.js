a7180df955769063c885d1469d275c73dc1b03c6
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2022-10-20 15:20:04 +0530

    Add More Functionality to textutils textForm page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <Link className="nav-link" to="/">{props.title}</Link>
+                <Link className="navbar-brand mb-0 h1" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
